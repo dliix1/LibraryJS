710584a5ec4436d85190b66d31b7b496de7e99ee
Zuhdija Delic <zuka_delic@hotmail.com>
2020-02-10 20:58:59 +0100

    Processing

---
+++ script.js
@@ -0,0 +1,95 @@
+let korisnik = new Array();
+let knjiga = new Array();
+window.onload = menu();
+
+function unos(){
+
+    let unosKorisnika = document.getElementById("unos").value;
+
+    switch (unosKorisnika) {
+		case "1":
+			kreiranjeRacuna();
+			break;
+
+		case "2":
+			kreiranjeKnjige();
+			break;
+
+		case "3":
+			podizanjeKnjige();
+			break;
+
+		case "4":
+			vracanjeKnjige();
+			break;
+
+		case "5":
+			ispisRacuna();
+			break;
+}
+
+function kreiranjeRacuna(){
+
+    let imeRacuna = prompt("Unesite ime racuna:", " ");
+    let brojRacuna = prompt ("Unesite broj racuna:", " ");
+
+    if (validacijaZaKreacijuRacuna(imeRacuna, brojRacuna)){
+        alert("Racun je uspjesno napravljen!");
+
+        korisnik.push(imeRacuna, brojRacuna);
+
+    }
+    
+}
+
+function validacijaZaKreacijuRacuna(){
+
+    for (let i = 0; i < korisnik.length(); i++){
+        if (korisnik[i] == imeRacuna){
+            alert ("Vec postoji racun sa unesenim imenom!")
+            return false;
+
+        }
+        else if(korisnik[i] == brojRacuna)
+        alert ("Vec postoji racun sa unesenim brojem!")
+        return false;
+
+    }
+    return true;
+
+}
+
+function kreiranjeKnjige(){
+
+    let imeKnjige = prompt ("Unesite ime knjige:", " ");
+    let brojKnjige = prompt ("Unesite broj knjige:", " ")
+    let statusKnjige = false;
+
+    if (validacijaZaKreacijuKnjige(imeKnjige, brojKnjige)){
+        alert("Knjiga je uspjesno napravljena!");
+
+        knjiga.push(imeKnjige, brojKnjige);
+    }
+
+}
+
+function validacijaZaKreacijuKnjige(){
+
+    for (let i = 0; i < knjiga.length; i++){
+        if (knjiga[i] == imeKnjige){
+            alert ("Vec postoji knjiga sa unijetim imenom!")
+            return false;
+
+        }
+
+        else if (knjiga[i] == brojKnjige){
+            alert ("Vec postoji knjiga sa unijetim brojem!")
+            return false;
+
+        }
+
+        return true;
+    }
+    
+}
+}
